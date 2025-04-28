466b253fecdb897b7839dbb96615e3ea3b6f5810
missfionajean <missfionajean@gmail.com>
2025-04-28 15:18:52 -0400

    removed unneeded promo component from sign-in pages

--- front-end/src/components/SignIn.tsx
+++ front-end/src/components/SignIn.tsx
@@ -1,63 +1,46 @@
-// this is the sign in page
-import Promo from "./Promo";
-
 interface SignInProps {
-    // declaring setPage as a function with type string and it wont return anything (void)
-    setPage: (page: string) => void;
+	// declaring setPage as a function with type string and it wont return anything (void)
+	setPage: (page: string) => void;
 }
 
 function SignIn({ setPage }: SignInProps) {
-    return (
-        <>
-            <section>
-                <h1>Sign In To Account</h1>
-                <form>
-                    <div>
-                        <label htmlFor="username">
-                            Username
-                        </label>
-                        <input
-                            type="text"
-                            name="username"
-                            id="username"
-                            placeholder="Enter username"
-                        />
-                    </div>
+	return (
+		<>
+			<h1>Sign In To Account</h1>
+			<form>
+				<div>
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						name="username"
+						id="username"
+						placeholder="Enter username"
+					/>
+				</div>
 
-                    <div>
-                        <label htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder="Enter password"
-                        />
-                    </div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="Enter password"
+					/>
+				</div>
 
-                    <a href="/" type="button">
-                        Cancel
-                    </a>
+				<a href="/" type="button">
+					Cancel
+				</a>
 
-                    <button type="submit">
-                        Sign In
-                    </button>
-                </form>
+				<button type="submit">Sign In</button>
+			</form>
 
-                <div>
-                    <h5>Don't have an account yet? </h5>
-                    <a onClick={() => setPage("Sign Up")}>
-                        Sign Up
-                    </a>
-                </div>
-            </section>
-
-            <section>
-                <Promo />
-            </section>
-        </>
-    )
-};
+			<div>
+				<h5>Don't have an account yet? </h5>
+				<a onClick={() => setPage("Sign Up")}>Sign Up</a>
+			</div>
+		</>
+	);
+}
 
 export default SignIn;
