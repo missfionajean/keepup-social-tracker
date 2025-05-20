f523513432e98a00b1532ccf126406c707754dd8
missfionajean <missfionajean@gmail.com>
2025-05-20 09:29:06 -0400

    defined CRUD url paths for contacts

--- back-end/controllers/contact.js
+++ back-end/controllers/contact.js
@@ -5,7 +5,7 @@ const Contact = require("../models/contact.js");
 const express = require("express");
 const router = express.Router();
 
-// defines sign-up path after "/auth"
+// path for getting all contacts
 router.get("/all", async (req, res) => {
 	if (!req.session.user) {
 		return res.send("You must be signed in to view this page.");
@@ -14,6 +14,46 @@ router.get("/all", async (req, res) => {
 		const allContacts = await Contact.find({ user: req.session.user._id });
 		res.json(allContacts);
 	}
+});
+
+// path for creating new contact
+router.post("/new", async (req, res) => {
+    if (!req.session.user) {
+        return res.send("You must be signed in to view this page.");
+    } else {
+        // add user id to contact data
+        req.body.user = req.session.user._id;
+
+        // create new contact in database
+        const newContact = await Contact.create(req.body);
+        res.json(newContact);
+    }
+});
+
+// path for updating a contact
+router.put("/update/:id", async (req, res) => {
+    if (!req.session.user) {
+        return res.send("You must be signed in to view this page.");
+    } else {
+        // update contact in database
+        const updatedContact = await Contact.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.json(updatedContact);
+    }
+});
+
+// path for deleting a contact
+router.delete("/delete/:id", async (req, res) => {
+    if (!req.session.user) {
+        return res.send("You must be signed in to view this page.");
+    } else {
+        // delete contact from database
+        const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+        res.json(deletedContact);
+    }
 });
 
 // export router for use in server.js
