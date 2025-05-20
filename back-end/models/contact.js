1d2869e0b3825d788d4ebe9e61d293003c478d88
missfionajean <missfionajean@gmail.com>
2025-05-20 08:35:14 -0400

    added contact mongoose model to backend

---
+++ back-end/models/contact.js
@@ -0,0 +1,15 @@
+// imports mongoose dependency
+const mongoose = require("mongoose");
+
+// schema for individual contact information
+const contactSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
+    lastContact: { type: String, required: true },
+    frequency: { type: String, required: true },
+});
+
+// properly encodes contact schema
+const Contact = mongoose.model("Contact", contactSchema);
+
+// exports contact schema
+module.exports = Contact;
